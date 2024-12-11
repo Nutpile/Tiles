ea0e8304296cf04a5c791275962f0e6d2911f91c
Nutpile <nutpile@hotmail.com>
2024-12-11 15:42:23 +0100

    Added selection inversion and made code prettier

--- src/main.c
+++ src/main.c
@@ -25,14 +25,18 @@ SOFTWARE.*/
 #include <stdlib.h> //for random tile generation
 #include <sys\rtc.h> //see above
 
+#define MAX_GAME_SIZE 5
+uint8_t gameGrid[MAX_GAME_SIZE][MAX_GAME_SIZE];
+
 // remember screen dimentions is 320x240
 
-uint8_t initGame(uint8_t difficulty)
-{
-    const uint8_t spacing = 10;   //spacing in pixels from the screen borders
-    const uint8_t size = 220; // size of the grid, most of the cases do not touch
-    gfx_SetColor(0);
+uint8_t difficulty = 0; // 0 is 3x3, 1 is 4x4, and 2 is 5x5, causes problems if set to over 2 for some reason
+const uint8_t spacing = 10; //spacing in pixels from the screen borders
+const uint8_t size = 220; // size of the grid, most of the cases do not touch
 
+uint8_t initGame()
+{
+    gfx_SetColor(0);
     //split square of size "size" in difficulty+3 parts since a 3x3 grid is the smallest it can be
     for(uint8_t i = 0; i<difficulty+4; i++){
         gfx_VertLine_NoClip(size/(difficulty+3)*i+spacing, spacing, size);
@@ -42,13 +46,19 @@ uint8_t initGame(uint8_t difficulty)
     return size/(difficulty+3);
 }
 
-void drawTile(uint8_t cellSize, uint8_t difficulty, uint8_t gameGrid[][difficulty+3], uint8_t row, uint8_t coloumn)
+void drawTile(uint8_t cellSize, uint8_t row, uint8_t coloumn)
 {
     if (gameGrid[row][coloumn] == 1) gfx_SetColor(24); // blue
     else gfx_SetColor(230); // yellow
 
-    //attention! the + 11 is the value of "spacing" from initGame() + 1
-    gfx_FillRectangle_NoClip(coloumn * cellSize + 11,row * cellSize + 11,cellSize-1,cellSize-1);
+    gfx_FillRectangle_NoClip(coloumn * cellSize + spacing + 1,row * cellSize + spacing + 1,cellSize-1,cellSize-1);
+}
+
+void click(uint8_t column, uint8_t row, uint8_t cellSize)
+{
+    //invert current tile, no if condition here since it's always possible
+    gameGrid[row][column] = !gameGrid[row][column];
+    drawTile(cellSize, row, column);
 }
 
 void select(uint8_t cellSize)
@@ -56,39 +66,46 @@ void select(uint8_t cellSize)
     bool quit = false;
     int8_t deltaX = 0;
     int8_t deltaY = 0;
-    uint8_t selectionX = 10;
-    uint8_t selectionY = 10;
+    uint8_t selectionX = 0;
+    uint8_t selectionY = 0;
     uint8_t OLDselectionX = selectionX;
     uint8_t OLDselectionY = selectionY;
 
     // Draw first selection
     gfx_SetColor(224);
-    gfx_Rectangle_NoClip(selectionX, selectionY, cellSize+1, cellSize+1);
+    gfx_Rectangle_NoClip(spacing, spacing, cellSize+1, cellSize+1);
 
     while (!quit)
     {
         uint8_t key = os_GetCSC();
+        bool clicked = false;
         if (key) // execute only if a key is pressed
         {
             switch (key)
             {
-                case sk_Down: deltaX = 0; deltaY = cellSize; break;
-                case sk_Up: deltaX = 0; deltaY = -cellSize; break;
-                case sk_Left: deltaX = -cellSize; deltaY = 0; break;
-                case sk_Right: deltaX = cellSize; deltaY = 0; break;
-                case sk_Clear: quit = true; continue; // Exit the loop
+                case sk_Enter: clicked = true; break;
+                case sk_Down: deltaX = 0; deltaY = 1; break;
+                case sk_Up: deltaX = 0; deltaY = -1; break;
+                case sk_Left: deltaX = -1; deltaY = 0; break;
+                case sk_Right: deltaX = 1; deltaY = 0; break;
+                case sk_Clear: quit = true; break; // Exit the loop
                 default: deltaX = 0; deltaY = 0; break;
             }
 
+            if (clicked) click(selectionX, selectionY, cellSize);
+
             // check bounds
-            int16_t NEWSelectionY = selectionY + deltaY;
-            int16_t NEWSelectionX = selectionX + deltaX;
+            uint8_t NEWSelectionY = selectionY + deltaY;
+            uint8_t NEWSelectionX = selectionX + deltaX;
+
+            deltaX = 0;
+            deltaY = 0;
 
             //the horror below is the only way my dumb ahh could code boundary detection
-            if (NEWSelectionX >= 10 && NEWSelectionX < 220 && NEWSelectionY >= 10 && NEWSelectionY < 220) {
+            if (NEWSelectionX >= 0 && NEWSelectionX < difficulty+3 && NEWSelectionY >= 0 && NEWSelectionY < difficulty+3) {
                 // remove old selection
                 gfx_SetColor(0); // black
-                gfx_Rectangle_NoClip(OLDselectionX, OLDselectionY, cellSize + 1, cellSize + 1);
+                gfx_Rectangle_NoClip(OLDselectionX*cellSize+spacing, OLDselectionY*cellSize+spacing, cellSize + 1, cellSize + 1);
 
                 // update selection coords
                 selectionX = NEWSelectionX;
@@ -96,7 +113,7 @@ void select(uint8_t cellSize)
 
                 // draw new selection
                 gfx_SetColor(224); // Red selection
-                gfx_Rectangle_NoClip(selectionX, selectionY, cellSize + 1, cellSize + 1);
+                gfx_Rectangle_NoClip(selectionX*cellSize+spacing, selectionY*cellSize+spacing, cellSize + 1, cellSize + 1);
 
                 // update old position
                 OLDselectionX = selectionX;
@@ -112,20 +129,17 @@ void select(uint8_t cellSize)
     }
 }
 
-
 int main(void)
 {
     srand(rtc_Time());
-    uint8_t difficulty = 0; // 0 is 3x3, 1 is 4x4, and 2 is 5x5, causes problems if set to over 2 for some reason
     gfx_Begin();
-    uint8_t cellSize = initGame(difficulty);
-    uint8_t gameGrid[difficulty+3][difficulty+3];
+    uint8_t cellSize = initGame();
     for(uint8_t i = 0; i < difficulty+3; i++)
     {
         for(uint8_t j = 0; j < difficulty+3; j++)
         {
             gameGrid[i][j] = rand() % 2; // Use % 2 to generate 0 or 1
-            drawTile(cellSize, difficulty, gameGrid, i, j);
+            drawTile(cellSize, i, j);
         }
     }
     select(cellSize);
